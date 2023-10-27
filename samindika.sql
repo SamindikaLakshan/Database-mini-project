cb84e4450e25bf6c1fd5314bab1ce5fdda8b2b69
samindikaLakshan <samindikalakshan71@gmail.com>
2023-10-27 14:21:17 +0530

    creating user accounts

---
+++ samindika.sql
@@ -0,0 +1,20 @@
+CREATE USER 'Admin'@'localhost'
+IDENTIFIED BY 'admin123';
+
+mysql -u Admin -p
+admin123
+
+CREATE USER 'Dean'@'localhost'
+IDENTIFIED BY 'dean123';
+
+CREATE USER 'Lecturer'@'localhost'
+IDENTIFIED BY 'lec123';
+
+CREATE USER 'Technicalofficer'@'localhost'
+IDENTIFIED BY 'to123';
+
+CREATE USER 'Student'@'localhost'
+IDENTIFIED BY 'student123';
+
+quit;
+
